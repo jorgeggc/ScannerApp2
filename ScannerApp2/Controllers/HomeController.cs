df612e3ffd98985fb240e080111ac8e7bc73dd91
friends family <jorge.baeza614@gmail.com>
2020-04-26 17:55:21 -0500

    border for good or bad

--- ScannerApp2/Controllers/HomeController.cs
+++ ScannerApp2/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace ScannerApp2.Controllers
     {
         //Window Autheniticaten 
         //For window users
-        [Authorize(Users = @"AUSB1\AUSBU1")]
+        [Authorize(Users = @"AUSB1\AUSBU2")]
         public ActionResult Index()
         {
             return View();
